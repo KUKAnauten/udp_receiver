652d758ca6c4e25a86d595ea55449be4346395e5
Niklas Schaefer <niklasjoel@gmx.net>
2018-04-27 20:13:25 +0200

    Uncoupling v1

--- scripts/pick_and_place_receive.py
+++ scripts/pick_and_place_receive.py
@@ -6,6 +6,7 @@ import rospy
 from geometry_msgs.msg import PoseStamped
 from iiwa_msgs.msg import JointPosition
 from std_msgs.msg import Float64
+from joint_follower.srv import *
 
 #Parameters - TODO make them CLI/ROS-Parameters
 UDP_IP = "212.212.21.2"
@@ -15,6 +16,14 @@ SAMPLE_RATE = 100
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
+def uncouple_client(uncouple_req):
+  rospy.wait_for_service('coupler')
+  try:
+    uncouple = rospy.ospy.ServiceProxy('coupler', Coupler)
+    response = uncouple(uncouple_req)
+  except ospy.ServiceException, e:
+    print "Service call failed: %s"%e
+
 def talker(poseTopicName='PoseStamped', jointTopicName='JointPosition', gripperCommandTopicName='GripperCommand'):
     pose_pub = rospy.Publisher('poseFromUDP/'+poseTopicName, PoseStamped, queue_size=10)
     joint_pub = rospy.Publisher('jointAnglesFromUDP/'+jointTopicName, JointPosition, queue_size=10)
@@ -22,9 +31,9 @@ def talker(poseTopicName='PoseStamped', jointTopicName='JointPosition', gripperC
     rospy.init_node('mcsPublisher', anonymous=True)
     rate = rospy.Rate(SAMPLE_RATE)
     while not rospy.is_shutdown():
-        data, addr = sock.recvfrom(120) # buffer size is (7+7+1)*8 bytes
-        values = struct.unpack('<ddddddddddddddd', data)
-        #rospy.loginfo(value)
+        data, addr = sock.recvfrom(128) # buffer size is (7+7+1+1)*8 bytes
+        values = struct.unpack('<dddddddddddddddd', data)
+        #rospy.loginfo(value)         
         pose = PoseStamped()
         pose.header.frame_id = "/operator"
         pose.header.stamp = rospy.Time.now()
@@ -49,6 +58,13 @@ def talker(poseTopicName='PoseStamped', jointTopicName='JointPosition', gripperC
 
         gripper_command = Float64()
         gripper_command.data = values[14]
+
+        if value[15]==1 and last_value==0:
+          uncouple_client(true)  
+        elif value[15]==0 and last_value==1:
+          uncouple_client(false)
+
+        last_value = value[15]
 
         pose_pub.publish(pose)
         joint_pub.publish(joints)
